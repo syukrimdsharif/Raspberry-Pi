d49d34507b4650a5c8be56784c780a7c06474726
Syukri Sharif <30360616+syukrimdsharif@users.noreply.github.com>
2017-10-03 21:54:50 +0800

    LED Blinking and Push Button
    The beginner tutorial for Raspberry Pi

---
+++ LED Blinking.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(23, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(23, GPIO.LOW)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
---
+++ Push Button and LED.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.IN, GPIO.PUD_UP)
+
+try:
+    while True:
+        if GPIO.input(24) == 0:
+            GPIO.output(23, GPIO.HIGH)
+        else:
+            GPIO.output(23, GPIO.LOW)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
